edbb3ade67c677b3f2ccb51bdea09e0c65f63115
Joana Palero <joana.palero24@gmail.com>
2018-05-31 01:39:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def conference_badges(names)
+  puts "Hello, my name is #{names}."
+end
+
+# badge_maker = ["Arel","Edsger", "Ada", "Charles", "Alan",  "Grace", "Linus", "Matz"]
+# badge_maker.each do |names|
+
+def badge_maker(names)
+  result=Array.new
+  name_array.each do |names|
+    result.push(badge_maker(names))
+  end
+  return result
+end
+
+def assign_rooms(names)
+  result=Array.new
+  names.each_with_index {|name,index| result.push("Hello, #{names}! You'll be assigned to room #{index+1}!")}
+  return result
+end
+
+def printer(names)
+  rooms = assign_rooms(names)
+  badges = badge_maker(names)
+  badges.each do |greeting|
+    puts greeting
+  end
+  rooms.each do |assignment|
+    puts assignment
+  end
+end
