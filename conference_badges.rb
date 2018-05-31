e5868d016b7fc56432147d1ae5b116de8d1edb16
Joana Palero <joana.palero24@gmail.com>
2018-05-31 01:46:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 
 def assign_rooms(name_array)
   result=Array.new
-  names.each_with_index {|name,index| result.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+  name_array.each_with_index {|name,index| result.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
   return result
 end
 
